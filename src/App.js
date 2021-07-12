8aa0851265137a41a3fb47e9aeec827cdcb7ce78
Lina <31240383+linabiel@users.noreply.github.com>
2021-07-12 20:13:47 +0100

    added import 'react-app-polyfill/stable' to App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import GameBox from './containers/GameBox';
 import {useEffect, useState} from 'react';
+import 'react-app-polyfill/stable'
 
 
 function App() {

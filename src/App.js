9d772aa7b73020fbfa149682874bbac84a874ed2
Lina <31240383+linabiel@users.noreply.github.com>
2021-07-13 14:38:20 +0100

    clean start

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import logo from './logo.svg';
 import './App.css';
 import GameBox from './containers/GameBox';
 import {useEffect, useState} from 'react';
-import 'react-app-polyfill/stable'
 
 
 function App() {
